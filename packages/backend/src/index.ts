7ba504f7030d0f70bfd440c0c2253209dffb7529
taxrakoto <rakotomaharo.tahina@gmail.com>
2026-08-21 11:22:33 +0300

    feat(scaffolder): add Gitea publishing integration

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,6 +16,7 @@ backend.add(import('@backstage/plugin-proxy-backend'));
 // scaffolder plugin
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-gitea'));
 backend.add(
   import('@backstage/plugin-scaffolder-backend-module-notifications'),
 );
